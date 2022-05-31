8b5ffff8b0e97ffb0718ea78a6d2520abfb4bb56
Honcharov Anton <honcharov.280987@gmail.com>
2022-05-31 15:31:45 +0300

    creat test

--- src/main/java/homeworkEighteen/RomanToInteger.java
+++ src/main/java/homeworkEighteen/RomanToInteger.java
@@ -1,4 +1,39 @@
 package homeworkEighteen;
 
+import java.util.HashMap;
+
 public class RomanToInteger {
+
+    public int romanToInt(String s) {
+
+        int i = 0;
+        int sum = 0;
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("I", 1);
+        map.put("IV", 4);
+        map.put("V", 5);
+        map.put("IX", 9);
+        map.put("X", 10);
+        map.put("XL", 40);
+        map.put("L", 50);
+        map.put("XC", 90);
+        map.put("C", 100);
+        map.put("CD", 400);
+        map.put("D", 500);
+        map.put("CM", 900);
+        map.put("M", 1000);
+
+        while (i < s.length()) {
+            if (i + 1 < s.length() && map.containsKey(s.substring(i, i + 2))) {
+                int num = map.get(s.substring(i, i + 2));
+                sum += num;
+                i += 2;
+            } else if (map.containsKey(s.substring(i, i + 1))) {
+                int num = map.get(s.substring(i, i + 1));
+                sum += num;
+                i += 1;
+            }
+        }
+        return sum;
+    }
 }
--- src/test/java/homeworkEighteenTest/RomanToIntegerTest.java
+++ src/test/java/homeworkEighteenTest/RomanToIntegerTest.java
@@ -1,6 +1,23 @@
 package homeworkEighteenTest;
 
+import homeworkEighteen.RomanToInteger;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class RomanToIntegerTest {
 
+    RomanToInteger romanToInteger = new RomanToInteger();
+
+    String inputOne = "III";
+    String inputTwo = "LVIII";
+    String inputThree = "MCMXCIV";
+
+    @Test
+    public void testRomanToInteger() {
+
+        Assert.assertEquals(3, romanToInteger.romanToInt(inputOne));
+        Assert.assertEquals(58, romanToInteger.romanToInt(inputTwo));
+        Assert.assertEquals(1994, romanToInteger.romanToInt(inputThree));
+    }
 }
 
